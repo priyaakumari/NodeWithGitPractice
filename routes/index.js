f1c19dfe071fdf46e6209f8ac175c941ba56b8f3
Priya Kumari <priyaprasad.407gmail.com>
2023-01-08 19:03:59 +0530

    added 1 more controller and route users

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,13 @@ const express=require('express');
 
 const router= express.Router();
 
+//require home_controller exports objects
+const homeController= require('../controllers/home_controllers');
+
 console.log("router loaded");
+
+//get action from controller home_controller.js file
+router.get('/',homeController.home);
+router.use('/users', require('./users'));
 
 module.exports=router;
