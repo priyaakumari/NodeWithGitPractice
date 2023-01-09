575b1c8a3ee704d767c5b0d709f5c6f1fa7fad95
Priya Kumari <priyaprasad.407gmail.com>
2023-01-09 23:00:02 +0530

    Added partials and bit learnt about layouts

--- controllers/users_controllers.js
+++ controllers/users_controllers.js
@@ -1,3 +1,6 @@
 module.exports.profile= function(req,res){
-    return res.end('<h1>Profile Page Loaded</h1>')
+    // return res.end('<h1>Profile Page Loaded</h1>')
+    return res.render('user_profile',{
+        title:'profile'
+    });
 }
--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const port=8000;
 //use express router
 app.use('/',require('./routes'));       //or we can write app.use('/',require('./routes/index.js')); both will go to index of routes
 
+//set up view engine i.e ejs
+app.set('view engine','ejs');
+app.set('views','./views');
+
+
 app.listen(port,function(err){
     if(err){
          console.log(`Error on running : ${err}`);
